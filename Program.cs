50597f8f7a33d3953599f22700fdcbfe15695559
Diego Castillo Castillo <dcastillo@Diegos-MacBook-Pro.local>
2021-01-19 15:04:34 -0500

    Adding first dummy project.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
